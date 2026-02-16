feb910267552c94bd17a5fa0aec7c58a022ac2f9
dheaarlla <dheaaurellia312@gmail.com>
2026-02-17 01:05:21 +0700

    nambahin exp

--- storage/barangMakanan.java
+++ storage/barangMakanan.java
@@ -2,5 +2,19 @@ package storage;
 
 public class barangMakanan extends Barang{
     private String exp;
+
+    public barangMakanan (String nama, String kode, int stok, String exp){
+        super(nama, kode, stok);
+        this.exp = exp;
+    }
+
+    public String getExp () {
+        return exp;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | %-10s | %-5d | Exp: %-10s", getNama(), getKode(), getStok(), getExp());
+    }
     
 }
